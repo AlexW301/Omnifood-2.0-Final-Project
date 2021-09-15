9768a5f016af4431f25edbe8aba91319723e67e1
AlexW301 <66179529+AlexW301@users.noreply.github.com>
2021-09-15 08:31:34 -0400

    update

--- js/script.js
+++ js/script.js
@@ -13,18 +13,18 @@ console.log(h1);
 
 ////////////////////////////////////////////////////////////
 // Set copyright year automatically
-const yearEl = document.querySelector('.year');
+const yearEl = document.querySelector(".year");
 const currentYear = new Date().getFullYear();
 yearEl.textContent = currentYear;
 
 ////////////////////////////////////////////////////////////
 // Make mobile navigation work
 
-const btnNavEl = document.querySelector('.btn-mobile-nav');
-const headerEl = document.querySelector('.header');
+const btnNavEl = document.querySelector(".btn-mobile-nav");
+const headerEl = document.querySelector(".header");
 
-btnNavEl.addEventListener('click', function() {
-  headerEl.classList.toggle('nav-open')
+btnNavEl.addEventListener("click", function () {
+  headerEl.classList.toggle("nav-open");
 });
 
 // Need to make mobile nav close when clicking a navigation link
@@ -32,17 +32,31 @@ btnNavEl.addEventListener('click', function() {
 ////////////////////////////////////////////////////////////
 // Smooth Scrolling animation
 
-const allLinks = document.querySelectorAll('a:link');
+const allLinks = document.querySelectorAll("a:link");
 
-allLinks.forEach(function(link) {
-  link.addEventListener('click', function(e) {
+allLinks.forEach(function (link) {
+  link.addEventListener("click", function (e) {
     e.preventDefault();
-    const href = link.getAttribute('href');
-    console.log(href)
-  })
-})
+    const href = link.getAttribute("href");
+    // Scroll back to top
+    if (href === "#")
+      window.scrollTo({
+        top: 0,
+        behavior: "smooth",
+      });
+    
+    // Scroll to other links
+    if (href !== "#" && href.startsWith("#")) {
+      const sectionEl = document.querySelector(href);
+      sectionEl.scrollIntoView({ behavior: "smooth" });
+    }
 
-
+    // Close mobile navigation
+    if (link.classList.contains('main-nav-link')) {
+      headerEl.classList.remove('nav-open');
+    }
+  });
+});
 
 ///////////////////////////////////////////////////////////
 // Fixing flexbox gap property missing in some Safari versions
