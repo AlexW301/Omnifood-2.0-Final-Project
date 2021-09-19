6e2d7829c560a1431135b581fcc627409bbdf090
AlexW301 <66179529+AlexW301@users.noreply.github.com>
2021-09-19 15:09:07 -0400

    update

--- index.js
+++ index.js
@@ -1,9 +1,48 @@
 const {readFile} = require('fs').promises;
+
 const express = require('express');
 const app = express();
+const port = 3000
 
-app.get("/", async (request, response) => {
-    response.send(await readFile('./index.html', 'utf8'));
+// Static Files
+app.use(express.static('public'));
+app.use('/css', express.static(__dirname + 'public/css'));
+app.use('/js', express.static(__dirname + 'public/js'));
+app.use('/img', express.static(__dirname + 'public/img'));
+
+
+// *** FOR EJS ONLY *** //////
+// Set Views
+// app.set('views', './')
+// app.set('view engine', 'ejs')
+
+
+app.get('', (req, res) => {
+    // res.send('hello world!!!!');
+    res.sendFile(__dirname + '/index.html');
+    // *** FOR EJS ONLY *** //////
+    // res.render('index', {text: 'This is EJS!!!!!'});
 });
 
-app.listen(process.env.PORT || 3000, () => console.log('App available on http://localhost:3000'));
+
+
+
+
+
+
+// Listen on port 3000
+app.listen(port, () => {console.info(`Listen on port ${port}`)})
+
+
+
+
+
+
+// app.get("/", async (request, response) => {
+//     app.use(express.static('css'));
+//     app.use(express.static('js'));
+//     app.use(express.static('img'));
+//     response.send(await readFile('./index.html', 'utf8'));
+// });
+
+// app.listen(process.env.PORT || 3000, () => console.log('App available on http://localhost:3000'));
